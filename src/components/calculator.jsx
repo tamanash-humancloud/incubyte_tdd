eec0a8ff13b341a5f658a795ab307b27dd485c56
tamanash-humancloud <tamanash.malkhandi@humancloud.co.in>
2025-01-17 11:48:02 +0530

    Added Calculator display and its children and written test case for Rendering the same.

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Calculator = () => {
+    const [firstNumber, setFirstNumber] = useState('')
+    const [secondNumber, setSecondNumber] = useState('')
+    const [operator, setOperator] = useState('')
+    const [display, setDisplay] = useState('')
+
   return (
     <div className='calculator' data-testid="calculator">
-      
+        <div className='display' data-testid="display">
+            {/* Display the complete equation the user wants to run */}
+            <div data-testid="equation">
+                {firstNumber} {operator} {secondNumber}
+            </div>
+            {/* Displays user inputs and results */}
+            <div data-testid="input-result">
+                {display}
+            </div>
+        </div>
     </div>
   )
 }
--- src/test/App.test.js
+++ src/test/App.test.js
@@ -3,7 +3,15 @@ import Calculator from '../components/calculator';
 
 describe('Calculator Component', () => {
     it('renders the Calculator component', () => {
-      render(<Calculator />);
-      expect(screen.getByTestId('calculator')).toBeInTheDocument(); 
+        render(<Calculator />);
+        expect(screen.getByTestId('calculator')).toBeInTheDocument(); 
+    });
+
+
+    it('renders the display and its children', () => {
+        render(<Calculator />);
+        expect(screen.getByTestId('display')).toBeInTheDocument();
+        expect(screen.getByTestId('equation')).toBeInTheDocument(); 
+        expect(screen.getByTestId('input-result')).toBeInTheDocument();  
     });
 })
